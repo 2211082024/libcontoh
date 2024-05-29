a40fc3382acab157126f8932013fe2a21f678391
Puti Hanifah Marsla <putihanifah16@gmail.com>
2024-05-29 11:43:02 +0800

    commit pertama

---
+++ src/helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace puti\libhallo;
+
+class helloworld{
+    public function sayHello($name){
+        return "hello $name welcome to php and composer to create your own library";
+    }
+}
+
+?>
